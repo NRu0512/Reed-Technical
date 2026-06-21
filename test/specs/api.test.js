96f246216f2630cd55337f5b9ed9c57b3f0b5d4b
James Andrew Perez <75008298+NRu0512@users.noreply.github.com>
2026-06-21 23:11:14 +0800

    Add CreateBooking API test

--- test/specs/api.test.js
+++ test/specs/api.test.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { expect } from 'chai'
+import { booking } from '../data/booking.data';
 
 describe('Restful-Booker', () => {
-    const baseUrl = 'https://restful-booker.herokuapp.com';
     const apiClient = axios.create({ validateStatus: () => true });
+    const baseUrl = 'https://restful-booker.herokuapp.com';
+    const headers = { 'Accept': 'application/json' };
 
     describe('GetBookingIds', () => {
         it('should return all booking ids', async () => {
@@ -21,12 +23,8 @@ describe('Restful-Booker', () => {
         it('should return the booking details', async () => {
             const bookingProperties = ['firstname', 'lastname', 'totalprice', 'depositpaid', 'bookingdates'];
             const bookingIds = (await apiClient.get(`${baseUrl}/booking`)).data;
-            const id = bookingIds[Math.floor(Math.random() * bookingIds.length)].bookingid;
-            const response = await apiClient.get(`${baseUrl}/booking/${id}`, {
-                headers: {
-                    'Accept': 'application/json'
-                }
-            });
+            const id = bookingIds[Math.floor(Math.random() * bookingIds.length)].bookingid; // get random booking
+            const response = await apiClient.get(`${baseUrl}/booking/${id}`, { headers: headers });
             expect(response.status).to.equal(200);
             expect(response.data).to.be.an('object');
             expect(response.data).to.include.keys(bookingProperties);
@@ -35,19 +33,31 @@ describe('Restful-Booker', () => {
         it('should return 404 on non-existing booking id', async () => {
             const bookingIds = new Set((await apiClient.get(`${baseUrl}/booking`)).data.map(b => b.bookingid));
             let id = -1;
-            for (let i = 0; i <= bookingIds[bookingIds.length-1]; i++) {
+            for (let i = 0; i <= bookingIds[bookingIds.length-1]; i++) {    // find non-existing booking id
                 if (!bookingIds.has(i)) {
                     id = i;
                     break;
                 }
             }
             if (id === -1) id = bookingIds[bookingIds.length-1] + 10;
-            const response = await apiClient.get(`${baseUrl}/booking/${id}`, {
-                headers: {
-                    'Accept': 'application/json'
-                }
-            });
+            const response = await apiClient.get(`${baseUrl}/booking/${id}`, { headers: headers });
             expect(response.status).to.equal(404);
+        })
+    })
+
+    describe('CreateBooking', () => {
+        it('should create a booking', async () => {
+            const response = await apiClient.post(`${baseUrl}/booking`, booking, { headers: headers });
+            expect(response.status).to.equal(200);
+            expect(response.data).to.be.an('object');
+            expect(response.data).to.have.property('bookingid');
+            expect(response.data).to.have.property('booking');
+            expect(response.data.booking).to.be.deep.equal(booking)
+        })
+
+        it('should return an error when no data is sent', async () => {
+            const response = await apiClient.post(`${baseUrl}/booking`, { });
+            expect(response.status).to.equal(500);
         })
     })
 })
